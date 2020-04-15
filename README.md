40b02b29742d45062f84716b250de5d1e3e8fe77
Rico Huijbers <rix0rrr@gmail.com>
2020-04-15 16:32:32 +0200

    Add running and daemon mode

--- README.md
+++ README.md
@@ -9,3 +9,98 @@ on whether you're installing from a registry or running `npm install <file>.tgz`
 This tool helps make testing the same as for your users.
 
 ## Usage
+
+Basic usage:
+
+```
+serve-npm-tarballs [options] [COMMAND [...]]
+
+Options:
+  --verbose, -v        Increase logging verbosity           [count] [default: 0]
+  --directory, -d      Serve all *.tgz files from the given directory
+                                                         [string] [default: "."]
+  --log, -l            Write logs to the given file                     [string]
+  --port, -p           Port number to serve on          [number] [default: 4873]
+  --log-level, -L      Log level to log to file with  [string] [default: "info"]
+  --hide-upstream, -H  Hide upstream packages matching this filename mask (may
+                       be repeated)                        [array] [default: []]
+  --hide-tarballs, -h  Hide all packages found in *.tgz in the --directory from
+                       upstream (hides all versions)                   [boolean]
+  --daemon, -D         Run as a daemon. Output environment variables to interace
+                       with the daemon on stdout, ready to be eval'ed  [boolean]
+```
+
+The tool can be used in two ways:
+
+* Runs a subcommand and wait for it to exit.
+* Run as a daemon
+
+## Run a subcommand
+
+Convenient if you just need to run a single script against a mock repo:
+
+```
+serve-npm-tarballs [options] ./some-script-that-uses-npm.sh
+```
+
+The subcommand will be run with a modified environment so that all invocations
+of `npm` will automatically hit the fake registry.
+
+## Daemon mode
+
+Appropriate for integrating into a more complex bash workflow:
+
+```
+eval $(serve-npm-tarballs [options] --daemon)
+trap "kill $SERVE_NPM_TARBALLS_PID" EXIT
+
+# ...continue script...
+```
+
+The main invocation will output `export VAR=value` statements to stdout,
+which can be eval'ed in a bash script. The server will continue to run
+in the background while your script does something else.
+
+The environment variables will configure NPM to hit the mock registry.
+
+Don't forget to kill the server before your script exits.
+
+# Packages and hiding
+
+## Packages served
+
+By default, packages from packed tarballs in a directory are served:
+
+```
+# Serve tarballs from directory
+serve-npm-tarballs -d DIRECTORY [...]
+```
+
+Will serve all files called `*.tgz` as NPM packages from the given directory
+from the repository (default: current directory). Additional packages can be
+published later on by running `npm publish`.
+
+## Hiding
+
+By default, all package versions that haven't been published into the mock
+repository are transparently downloaded from the upstream repository
+(`npmjs.com`).
+
+If you want to ensure some kind of isolation and prevent against versioning
+mistakes, you can prevent packages with certain names or name patterns from
+being downloaded from the upstream repository. If they're not found in the mock
+directory, then they won't be found in the registry at all (See [Verdaccio
+docs](https://verdaccio.org/docs/en/packages)).
+
+```
+# Prevent all packages named @mycorp/* from being proxied
+serve-npm-tarballs -d DIRECTORY -H @mycorp/\* [...]
+```
+
+You can also automatically prevent proxying for all package names found in
+the collection of tarballs:
+
+```
+# Prevent all other versions of packages in DIRECTORY from being proxied
+serve-npm-tarballs -d DIRECTORY -h
+```
--- serve-npm-tarballs.ts
+++ serve-npm-tarballs.ts
@@ -1,12 +1,16 @@
 import * as yargs from 'yargs';
 import * as childProcess from 'child_process';
+import * as os from 'os';
+import * as path from 'path';
+import * as tar from 'tar';
+import { promises as fs } from 'fs';
 
 // No types :(
 const { default: startVerdaccio } = require('verdaccio');
 
 async function main() {
   const argv = yargs
-    .usage('$0 [options] COMMAND [...]')
+    .usage('$0 [options] [COMMAND [...]]')
     .option('verbose', {
       alias: 'v',
       type: 'boolean',
@@ -18,7 +22,7 @@ async function main() {
       alias: 'd',
       type: 'string',
       requiresArg: true,
-      desc: 'Serve *.tgz files from the given directory',
+      desc: 'Serve all *.tgz files from the given directory',
       default: '.',
     })
     .option('log', {
@@ -27,6 +31,13 @@ async function main() {
       requiresArg: true,
       desc: 'Write logs to the given file',
     })
+    .option('port', {
+      alias: 'p',
+      type: 'number',
+      requiresArg: true,
+      desc: 'Port number to serve on',
+      default: 4873
+    })
     .option('log-level', {
       alias: 'L',
       type: 'string',
@@ -34,83 +45,213 @@ async function main() {
       desc: 'Log level to log to file with',
       default: 'info'
     })
+    .option('hide-upstream', {
+      alias: 'H',
+      type: 'string',
+      array: true,
+      requiresArg: true,
+      default: [],
+      nargs: 1,
+      desc: 'Hide upstream packages matching this filename mask (may be repeated)',
+    })
+    .option('hide-tarballs', {
+      alias: 'h',
+      type: 'boolean',
+      desc: 'Hide all packages found in *.tgz in the --directory from upstream (hides all versions)',
+    })
+    .option('daemon', {
+      alias: 'D',
+      type: 'boolean',
+      desc: 'Run as a daemon. Output environment variables to interace with the daemon on stdout, ready to be eval\'ed',
+    })
     .help()
     .strict()
     .version()
     .showHelpOnFail(false)
     .argv;
 
-  if (argv._.length === 0) {
-    throw new Error(`Usage: serve-npm-tarballs COMMAND`);
+  if (argv._.length === 0 && !argv.daemon) {
+    throw new Error(`Usage: serve-npm-tarballs COMMAND, or use --daemon`);
+  }
+  if (argv._.length > 0 && argv.daemon) {
+    throw new Error(`Give either a COMMAND, or specify --daemon, but not both.`);
   }
 
-  if (!argv.verbose) {
-    debug = () => undefined;
-  }
+  if (argv.daemon) {
+    if (process.send) {
+      // Child process
+      await runWithServer(async (npmConfigEnv) => {
+        const bashExports = Object.entries(npmConfigEnv)
+          .map(([key, value]) => `export ${key}=${value}`)
+          .join('\n');
 
-  const port = 6000;
+        process.send!(bashExports);
 
-  const configJson = {
-    storage: argv.directory,
-    uplinks: {
-      npmjs: {
-        url: 'https://registry.npmjs.org',
-        cache: false
-      }
-    },
-    max_body_size: '100mb',
-    publish: {
-      allow_offline: true
-    },
-    logs: [
-      ...argv.log ? [{ type: 'file', path: argv.log, format: 'pretty', level: argv["log-level"] }] : [],
-      ...argv.verbose ? [{ type: 'stderr', format: 'pretty', level: argv["log-level"] }] : [],
-    ],
-    packages: {
-      '@aws-cdk/core': {
-        access: '$all',
-        publish: '$all',
-      },
-      '**': {
-        access: '$all',
-        publish: '$all',
-        proxy: 'npmjs',
-      }
-    },
-  };
+        debug('Waiting for SIGINT, SIGTERM, SIGUSR1');
 
-  startVerdaccio(configJson, port, argv.directory, '0.1.0', 'serve-npm-tarballs+verdaccio', (webServer: any, addr: any, _pkgName: any, _pkgVersion: any) => {
-    webServer.listen(addr.port || addr.path, addr.host, async () => {
-      debug('Verdaccio running');
+        await Promise.race([
+          new Promise(ok => process.on('SIGINT', ok)),
+          new Promise(ok => process.on('SIGTERM', ok)),
+          new Promise(ok => process.on('SIGUSR1', ok)),
+        ]);
 
+        debug('Shutting down.');
+      });
+    } else {
+      const daemonProc = childProcess.fork(path.resolve(__filename), process.argv.slice(2), {
+        detached: true,
+        stdio: ['ignore', 'ignore', 'ignore', 'ipc'],
+      });
+
+      // Wait for the first message, print that to stdout, then exit
+      const childMessage: string = await new Promise(ok => daemonProc.on('message', ok));
+      process.stdout.write(childMessage + '\n');
+
+      // Don't wait for the child proc anymore
+      daemonProc.disconnect();
+      daemonProc.unref();
+    }
+  } else {
+    // Not in daemon mode
+    await runWithServer(async (npmConfigEnv) => {
       // Disable SIGINT handling just before starting the subprocess, so the subprocess
       // completely gets to decide what to do with Ctrl-C.
       process.on('SIGINT', () => undefined);
-      try {
-        await invokeSubprocess(argv._, {
-          verbose: argv.verbose > 0
-        });
 
-        debug('Subprocess finished');
-      } catch (e) {
-        console.error(e.message);
-        process.exitCode = 1;
+      await invokeSubprocess(argv._, {
+        verbose: argv.verbose > 0,
+        env: {...process.env, ...npmConfigEnv},
+      });
+
+      debug('Subprocess finished');
+    });
+  }
+
+  async function runWithServer(action: (subprocessEnv: Record<string, string>) => Promise<void>) {
+    if (!argv.verbose) {
+      debug = () => undefined;
+    }
+
+    const port = argv.port;
+
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'serve-npm-tarballs-'));
+    try {
+      debug(`Working directory: ${tempDir}`);
+
+      const tarballs = (await fs.readdir(argv.directory, { encoding: 'utf-8' }))
+        .filter(f => f.endsWith('.tgz'))
+        .map(f => path.resolve(argv.directory, f));
+
+      const packagesToHide: string[] = [...argv["hide-upstream"]];
+      if (argv["hide-tarballs"]) {
+        // We have to read the package name from every individual tarball
+        for (const tarball of tarballs) {
+          try {
+            const pj = JSON.parse((await extractFileFromTarball(tarball, 'package/package.json')).toString());
+            packagesToHide.push(pj.name as any);
+          } catch (e) {
+            debug(`Error reading ${tarball}'s package.json: ${e.message}`);
+          }
+          tar.x({ file: tarball }, ['package/package.json']);
+        }
       }
 
-      webServer.close();
-    });
-  });
+      const packageHidingConfig: any = {};
+      debug(`Hiding ${packagesToHide}`);
+      for (const mask of packagesToHide) {
+        packageHidingConfig[mask] = {
+          access: '$all',
+          publish: '$all',
+          // Specifically: no 'proxy' directive!
+        };
+      }
+
+      const configJson = {
+        storage: tempDir,
+        uplinks: {
+          npmjs: {
+            url: 'https://registry.npmjs.org',
+            cache: false
+          }
+        },
+        max_body_size: '100mb',
+        publish: {
+          allow_offline: true
+        },
+        logs: [
+          ...argv.log ? [{ type: 'file', path: argv.log, format: 'pretty', level: argv["log-level"] }] : [],
+          ...argv.verbose ? [{ type: 'stderr', format: 'pretty', level: argv["log-level"] }] : [],
+        ],
+        packages: {
+          ...packageHidingConfig,
+          '**': {
+            access: '$all',
+            publish: '$all',
+            proxy: 'npmjs',
+          }
+        },
+      };
+
+      // Write a config file for NPM
+      // The auth token MUST be passed via .npmrc: https://github.com/npm/npm/issues/15565
+      await fs.writeFile(path.join(tempDir, '.npmrc'), [
+        `//localhost:${port}/:_authToken=none`,
+        '',
+      ].join('\n'), { encoding: 'utf-8' });
+
+      const npmConfigVars = {
+        // NPM should find the auth token here
+        npm_config_userconfig: path.join(tempDir, '.npmrc'),
+
+        // Pass registry via environment variable, so that if this script gets run via 'npm run'
+        // and all $npm_config_xxx settings are passed via environment variables, we still
+        // get to override it (the file would normally be ignored in that case).
+        npm_config_registry: `http://localhost:${port}/`,
+
+        // The PID which a script can use to kill us
+        SERVE_NPM_TARBALLS_PID: `${process.pid}`
+      };
+
+      const subprocessEnv = {
+        ...process.env,
+        ...npmConfigVars,
+      };
+
+      await new Promise(ok => startVerdaccio(configJson, port, tempDir, '0.1.0', 'serve-npm-tarballs+verdaccio', (webServer: any, addr: any, _pkgName: any, _pkgVersion: any) => {
+        debug(`Listening on ${addr.host}:${addr.port}`);
+        webServer.listen(addr.port || addr.path, addr.host, async () => {
+          try {
+            // Publish all tarballs
+            await Promise.all(tarballs.map(async (tarball) => invokeSubprocess(['npm', '--loglevel', 'silent', 'publish', tarball], {
+              verbose: argv.verbose > 0,
+              env: subprocessEnv,
+            })));
+
+            await action(npmConfigVars);
+          } catch (e) {
+            console.error(e.message);
+            process.exitCode = 1;
+          }
+
+          webServer.close();
+          ok();
+        });
+      }));
+    } finally {
+      await fs.rmdir(tempDir, { recursive: true });
+    }
+  }
 }
 
 let debug = (message: string) => {
   console.error('>', message);
 };
 
-export interface ShellOptions extends childProcess.SpawnOptions {
+interface ShellOptions extends childProcess.SpawnOptions {
   verbose?: boolean;
 }
 
-export async function invokeSubprocess(command: string[], options: ShellOptions = {}): Promise<string> {
+async function invokeSubprocess(command: string[], options: ShellOptions = {}): Promise<string> {
   if (options.verbose) {
     debug(`Executing '${command.join(' ')}'`);
   }
@@ -130,6 +271,20 @@ export async function invokeSubprocess(command: string[], options: ShellOptions 
       }
     });
   });
+}
+
+async function extractFileFromTarball(tarball: string, filePath: string): Promise<Buffer> {
+  const data = new Array<Buffer>();
+  await tar.t({
+    file: tarball,
+    onentry: entry => {
+      if (entry.path as unknown as string === filePath) {
+        entry.on('data', c => data.push(c));
+      }
+    }
+  });
+
+  return Buffer.concat(data);
 }
 
 
